6b7943977636061a0f19ebffdb44f86e423d2e91
victorcesi <victor.personnettaz@viacesi.fr>
2017-06-22 18:49:11 +0200

    commit test

---
+++ helloworldmvc/helloworldmvc.Contract/src/main/java/helloworldmvc/contract/IView.java
@@ -0,0 +1,5 @@
+package helloworldmvc.contract;
+
+public interface IView {
+
+}
---
+++ helloworldmvc/helloworldmvc.model/src/main/java/helloworldmvc/model/Model.java
@@ -0,0 +1,10 @@
+package helloworldmvc.model;
+
+import helloworldmvc.contract.*;
+
+public class Model implements IModel {
+	public String getHelloWorld() {
+		return null;
+	}
+
+}
